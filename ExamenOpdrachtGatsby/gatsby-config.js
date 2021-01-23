7767c84d1e7ad2a102281b1c7808f559bd80dbec
Jens Spiette <s108491@ap.be>
2021-01-23 14:54:46 +0100

    plugins gatsby

--- ExamenOpdrachtGatsby/gatsby-config.js
+++ ExamenOpdrachtGatsby/gatsby-config.js
@@ -27,6 +27,34 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
+    {
+
+      resolve: "gatsby-source-graphql",
+      
+      options: {
+      
+      typeName: "WPGraphql",
+      
+      fieldName: "wpcontent",
+      
+      url: "http://examenopdracht.local/graphql",     
+      },
+      
+      },
+      {
+
+        resolve: `gatsby-plugin-google-fonts`,
+        
+        options: {
+        
+        fonts: [`Roboto`, `Oswald`],
+        
+        display: "swap",
+        
+        },
+        
+        }, 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
